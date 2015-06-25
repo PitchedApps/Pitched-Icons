25cf7aa8ed4f9560cbb43a9cd6d954a14a4af78c
asdfasdfvful <asdfasdfvful@gmail.com>
2015-06-24 21:24:57 -0700

    initial commit

---
+++ app/src/main/java/com/pitchedapps/icons/launchers/KkLauncher.java
@@ -0,0 +1,16 @@
+package com.pitchedapps.icons.launchers;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.pitchedapps.icons.R;
+
+public class KkLauncher {
+    public KkLauncher(Context context) {
+        Intent kkApply = new Intent("com.kk.launcher.APPLY_ICON_THEME");
+        kkApply.putExtra("com.kk.launcher.theme.EXTRA_PKG", context.getPackageName());
+        kkApply.putExtra("com.kk.launcher.theme.EXTRA_NAME", context.getResources().getString(R.string.app_name));
+        context.startActivity(kkApply);
+    }
+
+}
